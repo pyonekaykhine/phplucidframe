15023e809fb456579ad9754ca71592c8836f9c90
Sithu <cithukyaw@gmail.com>
2020-04-19 22:42:01 +0630

    Remove useless comments

--- app/example/blog-page/query.php
+++ app/example/blog-page/query.php
@@ -15,36 +15,14 @@ $blog->slug  = 'custom-routing-to-a-page-including-a-form-example';
  $blog = db_select('post')->where('id', $id)->getSingleResult();
 
  //// OR
- //// You can also use `db_fetchResult()` with SQL which returns the std object
+ //// You can also use `db_fetchResult()` with raw SQL and it will return std object
 
  $sql = 'SELECT *, postTitle title FROM ' . db_table('post') . ' WHERE postId = :id';
  $blog = db_fetchResult($sql, array(':id' => $id));
 */
 
-//// Retrieving the result count
-//$sql = 'SELECT COUNT(*) FROM ' . db_table('post');
-//$count = db_count($sql);
-//echo $count . '<br>';
-//
-//$count = db_count('post')->fetch();
-//echo $count . '<br>';
-//
-//// Retrieving a field
-//$sql = 'SELECT MAX(postId) FROM ' . db_table('post');
-//$max = db_fetch($sql);
-//echo $max . '<br>';
-//
-//$sql = 'SELECT * FROM ' . db_table('post');
-//$result = db_query($sql);
-//
-//echo db_numRows($result);
-//
-//$sql = 'SELECT slug, postTitle FROM '. db_table('post') . ' WHERE postId = 3';
-//$post = db_fetchResult($sql);
-//_dpr($post);
-//exit;
-
 if ($blog) {
+    // Routing system make $slug available according to the route definition in inc/route.config.php
     if ($slug && strcasecmp($slug, $blog->slug) !== 0) {
         # 301 redirect to the correct URL
         _redirect301(_url('blog', array($id, $blog->slug)));
